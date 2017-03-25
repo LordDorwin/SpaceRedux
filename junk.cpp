c59c67d150ad9a625843b8b096dfef820e2e6764
Ryan Schouweiler <rybluemip@gmail.com>
2017-03-25 07:54:46 -0700

    Began reworking movement code

---
+++ junk.cpp
@@ -0,0 +1,34 @@
+//junk.cpp
+//Temporary code storage
+
+
+
+
+
+//Apply thrust to heading (accounting for time spent in frame
+//printf("%d", frameTime);
+printf("pre: %d, %d\n", heaVecX, heaVecY);
+heaVecX = (heaVecX / frameTime) + thrVecX;
+heaVecY = (heaVecY / frameTime) + thrVecY;
+printf("post: %d, %d\n", heaVecX, heaVecY);
+
+//Calculate resulting heading
+heaSpeed = sqrt((heaVecX*heaVecX) + (heaVecY*heaVecY));
+heaDir = radDeg(acos(heaVecX / heaSpeed));
+
+//Limit speed
+if (heaSpeed > maxSpeed) {
+	heaVecX = maxSpeed * radDeg(abs(cos(heaDir))) * sign(thrVecX);
+	heaVecY = maxSpeed * radDeg(abs(sin(heaDir))) * sign(thrVecY);
+	printf("shits fucked");
+}
+printf("%lf,  %lf\n", thrVecX, thrVecY);
+printf("%lf,  %lf\n", heaVecX, heaVecY);
+printf("\n");
+
+//Apply speed
+_x += heaVecX * frameTime;
+_y += heaVecY * frameTime;
+
+//Reset thrust vectors
+thrVecX = 0, thrVecY = 0;
